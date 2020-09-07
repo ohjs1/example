963375f5c249cb13944fe48d651c34e186ffd142
js oh <jsoh@jsui-MacBookPro.local>
2020-09-07 15:46:43 +0900

    문제5번수정

--- src/my_project/exam5/MainExcute.java
+++ src/my_project/exam5/MainExcute.java
@@ -18,20 +18,30 @@ public class MainExcute {
 		};
 		
 		int cnt = 0;
-		
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<arr.length; j++) {
-				if(arr[i][j] != 0) {
-					cnt++;
-					
-					//상하좌우검사해서 물깊이 1증가
-					if(arr[i][j] >= arr[i][j-1] && arr[i][j] >= arr[i-1][j] 
-							&& arr[i][j] >= arr[i][j+1] && arr[i][j] >= arr[i+1][j]) {
-						arr[i][j]++;
-						cnt++;
+		for(int k=0; k<4; k++) {
+			for(int i=0; i<arr.length; i++) {
+				for(int j=0; j<arr.length; j++) {
+					if(arr[i][j] != 0) {
+						
+						//상하좌우검사해서 물깊이 1증가
+						if(arr[i][j] <= arr[i][j-1] && arr[i][j] <= arr[i-1][j] 
+								&& arr[i][j] <= arr[i][j+1] && arr[i][j] <= arr[i+1][j]) {
+
+							arr[i][j]++;
+							
+						}
 					}
 				}
 			}
+		}
+		
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr.length; j++) {
+				System.out.print(arr[i][j] + " ");
+				cnt += arr[i][j];
+			}
+			System.out.println();
 		}
 		
 		System.out.println("물의 총 깊이 : " + cnt);
